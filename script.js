f5b9e7a195ad2f3a6b7d9ba9c24235a0db6f549b
Aktan Dzhusupov <dzhusupov_ak@au.kg>
2025-11-18 09:01:59 +0600

    fix: optimized

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const name = 'Aktan'
 
-// localStorage.setItem('user', JSON.stringify(user))
-
 const newUser = localStorage.getItem('user')
-console.log(JSON.parse(newUser))
-
 
 localStorage.clear()
